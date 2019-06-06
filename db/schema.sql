086bdc981f964e4bc65a0e79d99caa8a1c56b75b
King Conch <kbmajor80@gmail.com>
2019-06-05 23:17:34 -0400

    Updated seeds file with more rows and personal info for checking true email messaging.

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+### Schema
+
+DROP DATABASE IF EXISTS petstterz_db;
+
+CREATE DATABASE petstterz_db;
+USE petstterz_db;
+
+CREATE TABLE users(
+
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  image VARCHAR(255) NOT NULL,
+  street_address VARCHAR(255) NOT NULL,
+  zip_code INT(5) NOT NULL,
+  email_address VARCHAR(50) NOT NULL,
+  users_name VARCHAR(20) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  provider BOOLEAN DEFAULT false,
+  PRIMARY KEY (id)
+
+);
